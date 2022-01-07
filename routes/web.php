8df220fa940407b30975721fdc411ed9cbf04495
Haxorsprogramming <haxors@uinsu.ac.id>
2022-01-07 20:22:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\C_Home;
 
 Route::get('/', [C_Home::class, 'homePage']);
-Route::get('/manja', [C_Home::class, 'homePage']);
+// auth 
+Route::get('/login', [C_Home::class, 'homePage']);
