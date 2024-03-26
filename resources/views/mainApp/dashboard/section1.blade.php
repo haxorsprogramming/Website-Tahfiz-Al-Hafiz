ed2fe25563af2561193d51fa61ba12996e7a5589
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2024-03-26 22:55:49 +0800

    update

--- resources/views/mainApp/dashboard/section1.blade.php
+++ resources/views/mainApp/dashboard/section1.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="card-wrap">
                 <div class="card-header">
-                    <h4>Total Santri</h4>
+                    <h4>Total Santriya</h4>
                 </div>
                 <div class="card-body">
                     {{ $tSantri }}
